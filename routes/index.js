9eeb862ed1fbb105b26b14249a6709e96a5a0b1d
Philip Xu <philipxjm1@gmail.com>
2014-06-24 08:48:25 -0400

    redundant code removed

--- routes/index.js
+++ routes/index.js
@@ -21,6 +21,12 @@ router.get('/u', function(req, res) {
 	});
 });
 
+router.get('/p', function(req, res) {
+  mongoose.Pours.find(function (err, pours) {
+    res.send(pours);
+  });
+});
+
 
 
 router.post('/newJSON', function(req, res){
@@ -74,69 +80,4 @@ router.post('/newJSON', function(req, res){
    		}
 	});
 });
-
-router.param('cid', function(req,res, next, cid){
-  req.cid = cid;
-  next();
-});
-
-router.param('username', function(req,res, next, username){
-  req.username = username;
-  next();
-});
-
-router.param('displayName', function(req,res, next, displayName){
-  req.displayName = displayName;
-  next();
-});
-
-router.param('location', function(req,res, next, location){
-  req.location = location;
-  next();
-});
-
-router.param('imageURL', function(req,res, next, imageURL){
-  req.imageURL = imageURL;
-  next();
-});
-
-router.post('/new/:cid/:username/:displayName/:location/:imageURL', function (req, res){
-  var user;
-  var session;
-  console.log("POST: ");
-  console.log(req.body);
-  user = new mongoose.Users({
-  	cid: req.user.cid,
-    username: req.user.username,
-    displayName: req.user.displayName,
-    location: req.user.location,
-    imageURL: req.user.imageURL
-  });
-  user.save(function (err) {
-    if (!err) {
-      return console.log("created");
-    } else {
-      return console.log(err);
-    }
-  });
-
-  Keen.client.addEvents({
-    "Users": [
-	    {cid: req.user.cid,
-	    	username: req.user.username,
-	    	displayName: req.user.displayName,
-	    	location: req.user.location,
-	    	imageURL: req.user.imageURL}
-	]}, function(err, res) {
-    if (err) {
-        console.log("Oh no, an error!");
-    } else {
-        console.log("Hooray, it worked!");
-    }
-  });
-
-  return res.send(user);
-});
-
-
 module.exports = router;
